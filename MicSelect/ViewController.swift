f917bd50e69524c821fedba2ad281f016d8f2b91
Chad Parker <cparker@rzsoftware.com>
2021-11-08 14:02:36 -0800

    Add centered label

--- MicSelect/ViewController.swift
+++ MicSelect/ViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var titleLabel = UILabel().configure {
+        $0.text = "MicSelect"
+        $0.textAlignment = .center
+        $0.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        initialize()
+    }
 
+    private func initialize() {
         view.backgroundColor = .systemGray
+
+        view.addSubview(titleLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 }
